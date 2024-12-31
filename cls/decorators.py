ea12fd0655bba3a2b7104e846585c12015c0b474
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2024-12-31 11:40:10 +0300

    reduce redundancy on decorators

--- cls/decorators.py
+++ cls/decorators.py
@@ -2,100 +2,69 @@ import sys
 import logging
 import requests
 
+# Set up logging
+logging.basicConfig(level=logging.ERROR)
+
+
+def handle_error(func_name, error, exit_message):
+    """Handle the error by logging it and printing an exit message."""
+    logging.error(f"An error occurred in {func_name}: {str(error)}", exc_info=True)
+    print(f"\033[41;30m{exit_message}\033[0m")
+    sys.exit()
+
+
 def handle_common_errors(func):
-    """Handle common errors"""
+    """Handle common errors."""
+
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except ValueError as value_e:
-            logging.error(f"An error occured in {func.__name__}: {str(value_e)}", exc_info=True)
-            print("\033[41;30mInvalid input or value error. Try again.\033[0m")
-            sys.exit()
-        except KeyboardInterrupt as keyboard_e:
-            logging.error(f"An error occured in {func.__name__}: {str(keyboard_e)}", exc_info=True)
-            print("\033[41;30mKeyboard interrupt. Exiting...\033[0m")
-            sys.exit()
-        except EOFError as eof_e:
-            logging.error(f"An error occured in {func.__name__}: {str(eof_e)}", exc_info=True)
-            print("\033[41;30mEOF error. Input cannot be empty.\033[0m")
-            sys.exit()
-        except KeyError as key_e:
-            logging.error(f"An error occured in {func.__name__}: {str(key_e)}", exc_info=True)
-            print("\033[41;30mKey error. The key doesn't exist.\033[0m")
-            sys.exit()
-        except FileNotFoundError as file_e:
-            logging.error(f"An error occured in {func.__name__}: {str(file_e)}", exc_info=True)
-            print("\033[41;30mFile not found error.\033[0m")
-            sys.exit()
+        except ValueError as error:
+            handle_error(
+                func.__name__, error, "Invalid input or value error. Try again."
+            )
+        except KeyboardInterrupt as error:
+            handle_error(func.__name__, error, "Keyboard interrupt. Exiting...")
+        except EOFError as error:
+            handle_error(func.__name__, error, "EOF error. Input cannot be empty.")
+        except KeyError as error:
+            handle_error(func.__name__, error, "Key error. The key doesn't exist.")
+        except FileNotFoundError as error:
+            handle_error(func.__name__, error, "File not found error.")
         except Exception as error:
-            logging.error(f"An unknown error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print("\033[41;30mAn unknown error occurred.\033[0m")
-            print(f"Error: {error}")
-            sys.exit()
+            handle_error(func.__name__, error, "An unknown error occurred.")
+
     return wrapper
 
+
 def handle_api_errors(func):
-    """Handle the api errors"""
+    """Handle the API-related errors."""
+
     def wrapper(*args, **kwargs):
         try:
             response = func(*args, **kwargs)
             return response
         except requests.exceptions.TooManyRedirects as error:
-            logging.error(f"An error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mToo many redirects. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "Too many redirects. Try again.")
         except requests.exceptions.InvalidURL as error:
-            logging.error(f"An error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mInvalid URL. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "Invalid URL. Try again.")
         except requests.exceptions.Timeout as error:
-            logging.error(f"An error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mTimeout error. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "Timeout error. Try again.")
         except requests.exceptions.ConnectionError as error:
-            logging.error(f"An error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mConnection error. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "Connection error. Try again.")
         except requests.exceptions.RequestException as error:
-            logging.error(f"An error occured in '{func.__name__}': {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mRequest error. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "Request error. Try again.")
         except requests.exceptions.HTTPError as error:
-            logging.error(f"An error occured in {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
+            logging.error(
+                f"An error occurred in {func.__name__}: {str(error)}", exc_info=True
+            )
+            print("+" + "-" * 50 + "+")
+            print("|" + " " * 50 + "|")
             print("\033[41;30mHTTP error. Try again.\033[0m")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
+            print("|" + " " * 50 + "|")
+            print("+" + "-" * 50 + "+")
             return response
         except Exception as error:
-            logging.error("An unknown error occured in"
-                            f" {func.__name__}: {str(error)}", exc_info=True)
-            print('+' + '-'*50 + '+')
-            print('|' + ' '*50 + '|')
-            print("\033[41;30mAn unknown error occurred.\033[0m")
-            print(f"Error: {error}")
-            print('|' + ' '*50 + '|')
-            print('+' + '-'*50 + '+')
-            sys.exit()
+            handle_error(func.__name__, error, "An unknown error occurred.")
+
     return wrapper
